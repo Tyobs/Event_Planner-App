c2bde2fd976c94e59a547e24b871c50b25942e38
Tyobs <ymaluwa@gmail.com>
2025-04-06 14:57:41 +0200

    #updating the window and the database database using SQLite

--- DataBase.py
+++ DataBase.py
@@ -1,69 +1,168 @@
 import sqlite3
-conn = sqlite3.connect('event_planner.db') #Connecting to database and creating it, if it is not created
-cursor = conn.cursor()
+import os.path
+
+DB_FILE = 'event_planner.db'
+def connect_db():
+    return sqlite3.connect(DB_FILE)
+
+#checking if database file exists in my project folder
+if not os.path.isfile(DB_FILE):
+    #executing thi block if the database in my folder does not exist by creating a new one
+    conn = sqlite3.connect('event_planner.db') #Connecting to database and creating it, if it is not created
+    cursor = conn.cursor()
 
 #creating tables in our database
 #creating an event table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS events(
-        event_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        event_name TEXT,
-        event_date TEXT,
-        even_time TEXT,
-        event_location TEXT,
-        event_description TEXT
-    )
-''')
-#creating a task table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS tasks (
-        task_id INTEGER,
-        event_id INTEGER,
-        task_name TEXT,
-        task_description TEXT,
-        task_due_date TEXT,
-        task_status TEXT,
-        FOREIGN KEY (event_id) REFERENCES events (event_id)
-    )
-''')
-#creating the guest table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS guest(
-        guest_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        event_id INTEGER,
-        guest_name TEXT,
-        guest_email TEXT,
-        guest_phone TEXT,
-        rsvp_status TEXT,
-        FOREIGN KEY (event_id) REFERENCES events (event_id)
-    )
-''')
-#creating the budgets table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS budgets(
-        budget_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        event_id INTEGER,
-        category TEXT,
-        amount REAL,
-        description TEXT,
-        FOREIGN KEY (event_id) REFERENCES events (event_id)
-    )
-''')
-#craeting a vendor table
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS vendor(
-        vendor_id INTEGER PRIMARY KEY AUTOINCREMENT,
-        vendor_name TEXT,
-        vendor_contact TEXT,
-        vendor_email TEXT,
-        vendor_type TEXT,
-        event_id INTEGER,
-        FOREIGN KEY (event_id) REFERENCES events (event_id)
-    )
-''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS events(
+            event_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            event_name TEXT,
+            event_date TEXT,
+            event_time TEXT,
+            event_location TEXT,
+            event_description TEXT
+        )
+    ''')
+    #creating a task table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS tasks (
+            task_id INTEGER,
+            event_id INTEGER,
+            task_name TEXT,
+            task_description TEXT,
+            task_due_date TEXT,
+            task_status TEXT,
+            FOREIGN KEY (event_id) REFERENCES events (event_id)
+        )
+    ''')
+    #creating the guest table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS guest(
+            guest_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            event_id INTEGER,
+            guest_name TEXT,
+            guest_email TEXT,
+            guest_phone TEXT,
+            rsvp_status TEXT,
+            FOREIGN KEY (event_id) REFERENCES events (event_id)
+        )
+    ''')
+    #creating the budgets table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS budgets(
+            budget_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            event_id INTEGER,
+            category TEXT,
+            amount REAL,
+            description TEXT,
+            FOREIGN KEY (event_id) REFERENCES events (event_id)
+        )
+    ''')
+    #craeting a vendor table
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS vendor(
+            vendor_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            vendor_name TEXT,
+            vendor_contact TEXT,
+            vendor_email TEXT,
+            vendor_type TEXT,
+            event_id INTEGER,
+            FOREIGN KEY (event_id) REFERENCES events (event_id)
+        )
+    ''')
 
-#commiting the changes and close the connection
-conn.commit()
-conn.close()
+    #commiting the changes and close the connection
+    conn.commit()
+    conn.close()
 
-print("YAY!😊 Database and tables created successfully!👌 ")
+    print("YAY!😊 Database and tables created successfully!👌 ")
+
+else:
+    print("Database already exists.!😊😊")
+
+#Creating the CRUD (Create,Read,Update, and Delete) operations for events table in my database
+def create_event(event_name, event_date, event_time, event_location, event_description):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT INTO events (event_name, event_date, event_time, event_location,event_description)
+    VALUES (?, ?, ?, ?, ?)
+    ''', (event_name, event_date, event_time, event_location, event_description))
+    conn.commit()
+    conn.close()
+#This is my function to read from events table
+def read_events():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM events')
+    events = cursor.fetchall()
+    conn.close()
+    return events
+#this is a fuction similar to above function but is used to read a selected event
+def read_event(event_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM events WHERE event_id = ?', (event_id,))
+    event = cursor.fetchone()
+    conn.close()
+    return event
+#this is the fuction to update data in the events table
+def update_event(event_id, event_name, event_date, event_time, event_location, event_description):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+    UPDATE events
+    SET event_name = ?, event_date = ?, event_time = ?, event_location = ?, event_description = ?
+    WHERE event_id = ?
+    ''', (event_name, event_date, event_time, event_location, event_description, event_id))
+    conn.commit()
+    conn.close()
+def delete_event(event_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM events WHERE event_id = ?', (event_id,))
+    conn.commit()
+    conn.close()
+#this is a function that will be used in creating a vendor
+def create_vendor(vendor_name, vendor_contact, vendor_email, vendor_type, event_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT INTO vendors (vendor_name, vendor_contact, vendor_email, vendor_type, event_id)
+    VALUES (?, ?, ?, ?, ?)
+    ''', (vendor_name, vendor_contact, vendor_email, vendor_type, event_id))
+    conn.commit()
+    conn.close()
+#this is the function that will be used to create a vendor information into our database
+def read_vendors(event_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM vendors WHERE event_id = ?', (event_id,))
+    vendors = cursor.fetchall()
+    conn.close()
+    return vendors
+#this function will be used to update a vendor's  information in our database
+def update_vendors(vendor_id, vendor_name, vendor_contact, vendor_email, vendor_type):
+    conn = connect_db()
+    cursor =  conn.cursor()
+    cursor.execute('''
+    UPDATE vendors
+    SET vendor_name = ?, vendor_contact = ?, vendor_email = ?, vendor_type = ?
+    WHERE vendor_id = ?
+    ''', (vendor_name, vendor_contact, vendor_email, vendor_type, vendor_id))
+    conn.commit()
+    conn.close()
+def delete_vendor(vendor_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM vendors WHERE vendor_id = ?', (vendor_id,))
+    conn.commit()
+    conn.close()
+
+
+
+
+
+create_event("Birthday party", "2025-12-25", "15:00", "My House", "A fun day where I was born")
+events = read_events()
+print(events)
--- window.py
+++ window.py
@@ -1,6 +1,32 @@
 import tkinter as tk
 from tkinter import *
+from tkinter import ttk
+from tkinter import font
+from tkinter import scrolledtext
+from tkinter import messagebox
+from tkinter import filedialog
+import os
+import shutil
+import re
+import time
+import random
+import string
+import json
+import base64
+import threading
+import subprocess
+import sys
+import ctypes
+import platform
 
 window = Tk()
-wel = Label(text="Hello, this is my new window", font="calibri"); wel.pack()
+window.title("Event Planner")
+window.geometry("800x600")
+window.resizable(True, True)
+window.iconbitmap("D:\LineX Icon Pack\eventbrite.png")
+window.configure(bg="#000300")
+window.attributes("-topmost", True)
+window.attributes("-alpha", 0.95)
+window.overrideredirect(False)
+window.wm_attributes("-toolwindow", True)
 window.mainloop()
