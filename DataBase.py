7135f9cb7111e735a5ad03888f20200a1ecf605a
Tyobs <ymaluwa@gmail.com>
2025-04-07 11:40:44 +0200

    This commit involves the updated database  CRUD functions
    now all crud operations will be invloving in creating tasks, budgetsm and guest, but the error handling hasn't been considered, it will be considered in the next update

--- DataBase.py
+++ DataBase.py
@@ -80,7 +80,9 @@ if not os.path.isfile(DB_FILE):
 else:
     print("Database already exists.!😊😊")
 
-#Creating the CRUD (Create,Read,Update, and Delete) operations for events table in my database
+#Creating the CRUD (Create,Read,Update, and Delete) operations for events, tasks, guests, budgets, and vendors tables
+
+#this function will be used to create an event
 def create_event(event_name, event_date, event_time, event_location, event_description):
     conn = connect_db()
     cursor = conn.cursor()
@@ -90,6 +92,7 @@ def create_event(event_name, event_date, event_time, event_location, event_descr
     ''', (event_name, event_date, event_time, event_location, event_description))
     conn.commit()
     conn.close()
+
 #This is my function to read from events table
 def read_events():
     conn = connect_db()
@@ -98,6 +101,7 @@ def read_events():
     events = cursor.fetchall()
     conn.close()
     return events
+
 #this is a fuction similar to above function but is used to read a selected event
 def read_event(event_id):
     conn = connect_db()
@@ -106,6 +110,7 @@ def read_event(event_id):
     event = cursor.fetchone()
     conn.close()
     return event
+
 #this is the fuction to update data in the events table
 def update_event(event_id, event_name, event_date, event_time, event_location, event_description):
     conn = connect_db()
@@ -117,12 +122,15 @@ def update_event(event_id, event_name, event_date, event_time, event_location, e
     ''', (event_name, event_date, event_time, event_location, event_description, event_id))
     conn.commit()
     conn.close()
+
+#this function will be used to delete an event
 def delete_event(event_id):
     conn = connect_db()
     cursor = conn.cursor()
     cursor.execute('DELETE FROM events WHERE event_id = ?', (event_id,))
     conn.commit()
     conn.close()
+
 #this is a function that will be used in creating a vendor
 def create_vendor(vendor_name, vendor_contact, vendor_email, vendor_type, event_id):
     conn = connect_db()
@@ -133,7 +141,8 @@ def create_vendor(vendor_name, vendor_contact, vendor_email, vendor_type, event_
     ''', (vendor_name, vendor_contact, vendor_email, vendor_type, event_id))
     conn.commit()
     conn.close()
-#this is the function that will be used to create a vendor information into our database
+
+#this is the function that will be used to read vendors vendor information into our database
 def read_vendors(event_id):
     conn = connect_db()
     cursor = conn.cursor()
@@ -141,6 +150,16 @@ def read_vendors(event_id):
     vendors = cursor.fetchall()
     conn.close()
     return vendors
+
+#this function will be used to read informationn of a single vendor
+def read_vendor(vendor_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM vendors WHERE vendor_id = ?', (vendor_id,))
+    vendor = cursor.fetchone()
+    conn.close()
+    return vendor
+
 #this function will be used to update a vendor's  information in our database
 def update_vendors(vendor_id, vendor_name, vendor_contact, vendor_email, vendor_type):
     conn = connect_db()
@@ -152,6 +171,8 @@ def update_vendors(vendor_id, vendor_name, vendor_contact, vendor_email, vendor_
     ''', (vendor_name, vendor_contact, vendor_email, vendor_type, vendor_id))
     conn.commit()
     conn.close()
+
+#This function will be used in deleting a vendor
 def delete_vendor(vendor_id):
     conn = connect_db()
     cursor = conn.cursor()
@@ -159,10 +180,149 @@ def delete_vendor(vendor_id):
     conn.commit()
     conn.close()
 
+# this functunction will be used to create a budget
+def create_budget(event_id, category, amount, description):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT INTO budgets (event_id, category, amount, description)
+    VALUES (?, ?, ?, ?)
+    ''', (event_id, category, amount, description))
+    conn.commit()
+    conn.close()
 
+#this one will be used to update the budget
+def update_budget(budget_id, category, amount, description):
+    conn =  connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+    UPDATE budgets
+    SET category = ?, amount = ?, description = ?
+    WHERE budget_id = ?
+    ''', (category, amount, description, budget_id))
+    conn.commit()
+    conn.close()
 
+#this one will be used to read a particular nudget withing an event with given event_id key
+def read_budget(budget_id):
+    conn =  connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM budgets WHERE budget_id = ?', (budget_id,))
+    budget = cursor.fetchone()
+    conn.close()
+    return budget
 
+#this one will be used to read all budgets from an event
+def read_budgets(event_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM budgets WHERE event_id = ?', (event_id,))
+    budgets = cursor.fetchall()
+    conn.close()
+    return budgets
 
-create_event("Birthday party", "2025-12-25", "15:00", "My House", "A fun day where I was born")
-events = read_events()
-print(events)
+#this function will be used to delete a particular budget within an event
+def delete_budget(budget_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM budgets WHERE budget_id = ?', (budget_id,))
+    conn.commit()
+    conn.close()
+
+#this function will be used to create a task in a particular event
+def create_task(event_id, task_name, task_description, task_due_date, task_status):
+    conn =  connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT INTO tasks (event_id, task_name, task_description, task_due_date, task_status)
+    VALUES (?, ?, ?, ?, ?)
+    ''', (event_id, task_name, task_description, task_due_date, task_status))
+    conn.commit()
+    conn.close()
+
+#this function will be reading a task from a particula event
+def read_task(task_id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tasks WHERE task_id = ?', (task_id,))
+    task = cursor.fetchone()
+    conn.close()
+    return task
+
+#this function will be readining all tasks within an event
+def read_tasks(event_id):
+    conn =  connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tasks WHERE event_id = ?', (event_id,))
+    tasks = cursor.fetchall()
+    conn.close()
+    return tasks
+
+#this function will be used to update a task in an event
+def update_task(task_id, task_name, task_description, task_due_date, task_status):
+    conn = connect_db()
+    cursor =  conn.cursor()
+    cursor.execute('''
+    UPDATE tasks
+    SET task_name = ?, task_description = ?, task_due_date = ?, task_status =?
+    WHERE task_id = ?
+    ''', (task_name, task_description, task_due_date, task_status, task_id))
+    conn.commit()
+    conn.close()
+
+#this function will be used to delete a task in an event
+def delete_task(task_id):
+    conn=  connect_db()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM tasks WHERE task_id = ?', (task_id,))
+    conn.commit()
+    conn.close()
+
+#this function will be used to create a guest in an event
+def  create_guest(event_id, guest_name, guest_email, guest_phone, rsvp_status):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+    INSERT INTO guest (event_id, guest_name, guest_email, guest_phone, rsvp_status)
+    VALUES (?, ?, ?, ?, ?)
+    ''', (event_id, guest_name, guest_email, guest_phone, rsvp_status))
+    conn.commit()
+    conn.close()
+
+#this function will be used to read all guests in an event
+def read_guests(event_id):
+    conn = connect_db()
+    cursor = conn. cursor()
+    cursor.execute('SELECT * FROM guest WHERE event_id = ?', (event_id,))
+    guests = cursor.fetchall()
+    conn.close()
+    return guests
+
+#thiss function will be used to read only one guest with a given key value as guest_id
+def read_guest(guest_id):
+    conn = connect_db()
+    cursor =  conn.cursor()
+    cursor.execute('SELECT * FROM guest WHERE guest_id = ?', (guest_id,))
+    guest = cursor.fetchone()
+    conn.close()
+    return guest
+
+#this function will be used in updating a guest
+def update_guest(guest_id, guest_name, guest_email, guest_phone, rsvp_status):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('''
+    UPDATE guest
+    SET guest_name = ?, guest_email = ?, guest_phone = ?, rsvp_status = ?
+    WHERE guest_id = ?
+    ''', (guest_name, guest_email, guest_phone, rsvp_status, guest_id))
+    conn.commit()
+    conn.close()
+
+#this one will be wused to dete a guest from an event
+def delete_guest(guest_id):
+    conn =  connect_db()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM guest WHERE guest_id = ?', (guest_id,))
+    conn.commit()
+    conn.close()
