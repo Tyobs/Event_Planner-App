cae26652bdab142edabce51a86820e235f160aaa
Tyobs <ymaluwa@gmail.com>
2025-04-14 14:33:53 +0200

    and the presentation contatins power points  of how the whole project has been handled

--- DataBase.py
+++ DataBase.py
@@ -1,5 +1,12 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import *
+from tkinter import messagebox
 import sqlite3
 import os.path
+
+from pywin32_testutil import non_admin_error_codes
+from wx.lib.agw.ultimatelistctrl import wxEVT_COMMAND_LIST_COL_END_DRAG
 
 DB_FILE = 'event_planner.db'
 def connect_db():
@@ -84,220 +91,348 @@ else:
 
 #this function will be used to create an event
 def create_event(event_name, event_date, event_time, event_location, event_description):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO events (event_name, event_date, event_time, event_location,event_description)
-    VALUES (?, ?, ?, ?, ?)
-    ''', (event_name, event_date, event_time, event_location, event_description))
-    conn.commit()
-    conn.close()
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('''
+        INSERT INTO events (event_name, event_date, event_time, event_location,event_description)
+        VALUES (?, ?, ?, ?, ?)
+        ''', (event_name, event_date, event_time, event_location, event_description))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"Sorry😒😒, failed to create an event {e}")
+        if conn:
+            conn.close()
+        return None
 
 #This is my function to read from events table
 def read_events():
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM events')
-    events = cursor.fetchall()
-    conn.close()
-    return events
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM events')
+        events = cursor.fetchall()
+        conn.close()
+        return events
+    except sqlite3.Error as e:
+        print(f"Unknown Error occurred while reading data😒{e}")
+        if conn:
+            conn.close()
+        return None
 
-#this is a fuction similar to above function but is used to read a selected event
+#this is a function similar to above function but is used to read a selected event
 def read_event(event_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM events WHERE event_id = ?', (event_id,))
-    event = cursor.fetchone()
-    conn.close()
-    return event
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM events WHERE event_id = ?', (event_id,))
+        event = cursor.fetchone()
+        conn.close()
+        return event
+    except sqlite3.Error as e:
+        print(f"Error occurred while retrieving information of {event_id}😒😒 Sorry {e}")
+        if conn:
+            conn.close()
+        return None
 
-#this is the fuction to update data in the events table
+#this is the function to update data in the events table
 def update_event(event_id, event_name, event_date, event_time, event_location, event_description):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('''
-    UPDATE events
-    SET event_name = ?, event_date = ?, event_time = ?, event_location = ?, event_description = ?
-    WHERE event_id = ?
-    ''', (event_name, event_date, event_time, event_location, event_description, event_id))
-    conn.commit()
-    conn.close()
-
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('''
+        UPDATE events
+        SET event_name = ?, event_date = ?, event_time = ?, event_location = ?, event_description = ?
+        WHERE event_id = ?
+        ''', (event_name, event_date, event_time, event_location, event_description, event_id))
+        conn.commit()
+        conn.close()
+    except sqlite3.error as e:
+        print(f"Unknown Error has occurred while trying to update the data🤨 {e}")
+        if conn:
+            conn.close()
+        return None
 #this function will be used to delete an event
 def delete_event(event_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('DELETE FROM events WHERE event_id = ?', (event_id,))
-    conn.commit()
-    conn.close()
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM events WHERE event_id = ?', (event_id,))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An error occurred while deleting an event😒😒🤨 {e}")
+        if conn:
+            conn.close()
+    return  None
+
 
 #this is a function that will be used in creating a vendor
 def create_vendor(vendor_name, vendor_contact, vendor_email, vendor_type, event_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO vendors (vendor_name, vendor_contact, vendor_email, vendor_type, event_id)
-    VALUES (?, ?, ?, ?, ?)
-    ''', (vendor_name, vendor_contact, vendor_email, vendor_type, event_id))
-    conn.commit()
-    conn.close()
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('''
+        INSERT INTO vendors (vendor_name, vendor_contact, vendor_email, vendor_type, event_id)
+        VALUES (?, ?, ?, ?, ?)
+        ''', (vendor_name, vendor_contact, vendor_email, vendor_type, event_id))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"A vendor hasn't been created, try again later 🤨🤨🤨 {e}")
+        if conn:
+            conn.close()
+    return None
 
 #this is the function that will be used to read vendors vendor information into our database
 def read_vendors(event_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM vendors WHERE event_id = ?', (event_id,))
-    vendors = cursor.fetchall()
-    conn.close()
-    return vendors
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM vendors WHERE event_id = ?', (event_id,))
+        vendors = cursor.fetchall()
+        conn.close()
+        return vendors
+    except sqlite3.Error as e:
+        print(f"Unknown Error occurred while reading the vendors  from event😒 {e}")
+        if conn:
+            conn.close()
+    return None
 
-#this function will be used to read informationn of a single vendor
+#this function will be used to read information of a single vendor
 def read_vendor(vendor_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM vendors WHERE vendor_id = ?', (vendor_id,))
-    vendor = cursor.fetchone()
-    conn.close()
-    return vendor
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM vendors WHERE vendor_id = ?', (vendor_id,))
+        vendor = cursor.fetchone()
+        conn.close()
+        return vendor
+    except sqlite3.Error as e:
+        print(f"Unable to review the vendor with vendor_id: {vendor_id}😒🤣 {e}")
+        if conn:
+            conn.close()
+    return  None
 
 #this function will be used to update a vendor's  information in our database
 def update_vendors(vendor_id, vendor_name, vendor_contact, vendor_email, vendor_type):
-    conn = connect_db()
-    cursor =  conn.cursor()
-    cursor.execute('''
-    UPDATE vendors
-    SET vendor_name = ?, vendor_contact = ?, vendor_email = ?, vendor_type = ?
-    WHERE vendor_id = ?
-    ''', (vendor_name, vendor_contact, vendor_email, vendor_type, vendor_id))
-    conn.commit()
-    conn.close()
+    try:
+        conn = connect_db()
+        cursor =  conn.cursor()
+        cursor.execute('''
+        UPDATE vendors
+        SET vendor_name = ?, vendor_contact = ?, vendor_email = ?, vendor_type = ?
+        WHERE vendor_id = ?
+        ''', (vendor_name, vendor_contact, vendor_email, vendor_type, vendor_id))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"Error occurred while trying to update the vendor.👽👽 {e}")
+        if conn:
+            conn.close()
+    return None
 
 #This function will be used in deleting a vendor
 def delete_vendor(vendor_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('DELETE FROM vendors WHERE vendor_id = ?', (vendor_id,))
-    conn.commit()
-    conn.close()
-
-# this functunction will be used to create a budget
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM vendors WHERE vendor_id = ?', (vendor_id,))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"Error occurred while trying to delete a vendor {e}")
+        if conn:
+            conn.close()
+    return None
+# this function will be used to create a budget
 def create_budget(event_id, category, amount, description):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO budgets (event_id, category, amount, description)
-    VALUES (?, ?, ?, ?)
-    ''', (event_id, category, amount, description))
-    conn.commit()
-    conn.close()
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('''
+        INSERT INTO budgets (event_id, category, amount, description)
+        VALUES (?, ?, ?, ?)
+        ''', (event_id, category, amount, description))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"Unknown Error occurred while trying to create a budget😒 {e}")
+        if conn:
+            conn.close()
+    return None
 
 #this one will be used to update the budget
 def update_budget(budget_id, category, amount, description):
-    conn =  connect_db()
-    cursor = conn.cursor()
-    cursor.execute('''
-    UPDATE budgets
-    SET category = ?, amount = ?, description = ?
-    WHERE budget_id = ?
-    ''', (category, amount, description, budget_id))
-    conn.commit()
-    conn.close()
+    try:
+        conn =  connect_db()
+        cursor = conn.cursor()
+        cursor.execute('''
+        UPDATE budgets
+        SET category = ?, amount = ?, description = ?
+        WHERE budget_id = ?
+        ''', (category, amount, description, budget_id))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An Error occurred while trying to update a budget😒 {e}")
+        if conn:
+            conn.close()
+    return  None
 
-#this one will be used to read a particular nudget withing an event with given event_id key
+#this one will be used to read a particular budget withing an event with given event_id key
 def read_budget(budget_id):
-    conn =  connect_db()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM budgets WHERE budget_id = ?', (budget_id,))
-    budget = cursor.fetchone()
-    conn.close()
-    return budget
-
+    try:
+        conn =  connect_db()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM budgets WHERE budget_id = ?', (budget_id,))
+        budget = cursor.fetchone()
+        conn.close()
+        return budget
+    except sqlite3.Error as e:
+        print(f"An Error occurred while trying to read a budget😒 {e}")
+        if conn:
+            conn.close()
+    return None
 #this one will be used to read all budgets from an event
 def read_budgets(event_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM budgets WHERE event_id = ?', (event_id,))
-    budgets = cursor.fetchall()
-    conn.close()
-    return budgets
-
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM budgets WHERE event_id = ?', (event_id,))
+        budgets = cursor.fetchall()
+        conn.close()
+        return budgets
+    except sqlite3.Error as e:
+        print(f"An Error occurred while reading budgets from an event👽👽 {e}")
+        if conn:
+            conn.close()
+    return None
 #this function will be used to delete a particular budget within an event
 def delete_budget(budget_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('DELETE FROM budgets WHERE budget_id = ?', (budget_id,))
-    conn.commit()
-    conn.close()
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM budgets WHERE budget_id = ?', (budget_id,))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"Error while trying to delete a budget from the budget table💀💀 {e}")
+        if conn:
+            conn.close()
+    return None
 
 #this function will be used to create a task in a particular event
 def create_task(event_id, task_name, task_description, task_due_date, task_status):
-    conn =  connect_db()
-    cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO tasks (event_id, task_name, task_description, task_due_date, task_status)
-    VALUES (?, ?, ?, ?, ?)
-    ''', (event_id, task_name, task_description, task_due_date, task_status))
-    conn.commit()
-    conn.close()
+    try:
+        conn =  connect_db()
+        cursor = conn.cursor()
+        cursor.execute('''
+        INSERT INTO tasks (event_id, task_name, task_description, task_due_date, task_status)
+        VALUES (?, ?, ?, ?, ?)
+        ''', (event_id, task_name, task_description, task_due_date, task_status))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An Error occurred while trying to create a task👽👽 {e}")
+        if conn:
+            conn.close()
+    return None
 
 #this function will be reading a task from a particula event
 def read_task(task_id):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM tasks WHERE task_id = ?', (task_id,))
-    task = cursor.fetchone()
-    conn.close()
-    return task
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM tasks WHERE task_id = ?', (task_id,))
+        task = cursor.fetchone()
+        conn.close()
+        return task
+    except sqlite3.Error as e:
+        print(f"An Error occurred while trying to read a task from tasks🤣🤣 {e}")
+        if conn:
+            conn.close()
+    return None
 
-#this function will be readining all tasks within an event
+#this function will be reading all tasks within an event
 def read_tasks(event_id):
-    conn =  connect_db()
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM tasks WHERE event_id = ?', (event_id,))
-    tasks = cursor.fetchall()
-    conn.close()
-    return tasks
+    try:
+        conn =  connect_db()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM tasks WHERE event_id = ?', (event_id,))
+        tasks = cursor.fetchall()
+        conn.close()
+        return tasks
+    except sqlite3.Error as e:
+        print(f"An Error occurred while trying to read all tasks from the event😒😒 {e}")
+        if conn:
+            conn.close()
+    return None
 
 #this function will be used to update a task in an event
 def update_task(task_id, task_name, task_description, task_due_date, task_status):
-    conn = connect_db()
-    cursor =  conn.cursor()
-    cursor.execute('''
-    UPDATE tasks
-    SET task_name = ?, task_description = ?, task_due_date = ?, task_status =?
-    WHERE task_id = ?
-    ''', (task_name, task_description, task_due_date, task_status, task_id))
-    conn.commit()
-    conn.close()
+    try:
+        conn = connect_db()
+        cursor =  conn.cursor()
+        cursor.execute('''
+        UPDATE tasks
+        SET task_name = ?, task_description = ?, task_due_date = ?, task_status =?
+        WHERE task_id = ?
+        ''', (task_name, task_description, task_due_date, task_status, task_id))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An error occurred while trying to update the task🤣🤣 {e}")
+        if conn:
+            conn.close()
+    return  None
 
 #this function will be used to delete a task in an event
 def delete_task(task_id):
-    conn=  connect_db()
-    cursor = conn.cursor()
-    cursor.execute('DELETE FROM tasks WHERE task_id = ?', (task_id,))
-    conn.commit()
-    conn.close()
+    try:
+        conn =  connect_db()
+        cursor = conn.cursor()
+        cursor.execute('DELETE FROM tasks WHERE task_id = ?', (task_id,))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"An Error occurred while trying to delete a particular task.❌🚫 {e}")
+        if conn:
+            conn.close()
+    return None
 
 #this function will be used to create a guest in an event
 def  create_guest(event_id, guest_name, guest_email, guest_phone, rsvp_status):
-    conn = connect_db()
-    cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO guest (event_id, guest_name, guest_email, guest_phone, rsvp_status)
-    VALUES (?, ?, ?, ?, ?)
-    ''', (event_id, guest_name, guest_email, guest_phone, rsvp_status))
-    conn.commit()
-    conn.close()
+    try:
+        conn = connect_db()
+        cursor = conn.cursor()
+        cursor.execute('''
+        INSERT INTO guest (event_id, guest_name, guest_email, guest_phone, rsvp_status)
+        VALUES (?, ?, ?, ?, ?)
+        ''', (event_id, guest_name, guest_email, guest_phone, rsvp_status))
+        conn.commit()
+        conn.close()
+    except sqlite3.Error as e:
+        print(f"Error while trying to create a guest🚫🚫 {e}")
+        if conn:
+            conn.close()
+    return None
 
 #this function will be used to read all guests in an event
 def read_guests(event_id):
-    conn = connect_db()
-    cursor = conn. cursor()
-    cursor.execute('SELECT * FROM guest WHERE event_id = ?', (event_id,))
-    guests = cursor.fetchall()
-    conn.close()
-    return guests
-
+    try:
+        conn = connect_db()
+        cursor = conn. cursor()
+        cursor.execute('SELECT * FROM guest WHERE event_id = ?', (event_id,))
+        guests = cursor.fetchall()
+        conn.close()
+        return guests
+    except sqlite3.Error as e:
+        print(f"Error while trying to read guests from the db🤣🤣🤣 {e}")
+        if conn:
+            conn.close()
+    return None
 #thiss function will be used to read only one guest with a given key value as guest_id
 def read_guest(guest_id):
     conn = connect_db()
@@ -319,7 +454,7 @@ def update_guest(guest_id, guest_name, guest_email, guest_phone, rsvp_status):
     conn.commit()
     conn.close()
 
-#this one will be wused to dete a guest from an event
+#this one will be used to delete a guest from an event
 def delete_guest(guest_id):
     conn =  connect_db()
     cursor = conn.cursor()
---
+++ functions.py
@@ -0,0 +1,19 @@
+print(f"""1. Buy bundle/airtime
+2. Send money
+3. Make Payments
+4. Withdraw Money
+5. Financial Services
+6. Bank
+7. Account/PIN
+8. E-Tickets""")
+selection = input("")
+selection = int(selection)
+if selection == 1: # selection = 1, if 1 == 1:
+    print(f"""1. Buy bundles
+2. Buy airtime
+3. go back""")
+elif selection == 2:
+else:
+    print('Wrong input')
+
+
